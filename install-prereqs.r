648468d17f17eff89066981944863d2fae8dd693
earvonen <101631823+earvonen@users.noreply.github.com>
2024-02-29 12:58:08 +0200

    Update install-prereqs.r

--- install-prereqs.r
+++ install-prereqs.r
@@ -1,4 +1,5 @@
 #install.packages("randomForest")
 #install.packages(c("ModelMetrics", "plyr", "pROC", "reshape2"))
+install.packages(c("RcppTOML", "reticulate", "tfruns", "tfautograph"))
 install.packages("keras")
 #install.packages("caret")
