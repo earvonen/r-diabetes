6ef6372410d239c8b7352af82faa02c0140db009
earvonen <101631823+earvonen@users.noreply.github.com>
2024-03-01 09:09:30 +0200

    Create export_to_tf.py

---
+++ export_to_tf.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.models import load_model
+
+# Load the Keras model from the HDF5 file
+model = load_model('diabetes.h5')
+
+# Define the path to save the TensorFlow SavedModel
+saved_model_dir = './diabetes.tf'
+
+# Save the model in TensorFlow SavedModel format
+model.save(saved_model_dir, save_format='tf')
