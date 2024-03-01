7c3f8cc89f75e4f348d9d4a8487a63d4109be8e3
earvonen <101631823+earvonen@users.noreply.github.com>
2024-03-01 09:07:56 +0200

    Create install-prereqs.sh

---
+++ install-prereqs.sh
@@ -0,0 +1,7 @@
+r install-prereqs.r
+
+/usr/bin/python3.9 -m venv ~/.virtualenvs/r-reticulate
+source ~/.virtualenvs/r-reticulate/bin/activate
+pip install --upgrade pip
+pip install tensorflow keras
+pip install tf2onnx
