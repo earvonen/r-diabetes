5eb8e4a6cbfe435f4a2aa71006c92ca6d73e09e5
earvonen <101631823+earvonen@users.noreply.github.com>
2024-03-01 09:04:51 +0200

    Update install-prereqs.r

--- install-prereqs.r
+++ install-prereqs.r
@@ -3,3 +3,7 @@
 install.packages(c("Rcpp", "RcppTOML", "reticulate", "tfruns", "tfautograph", "readr"))
 install.packages("keras")
 #install.packages("caret")
+
+library(reticulate)
+install_python(version = "3.9")
+use_virtualenv("r-reticulate", required=TRUE)
