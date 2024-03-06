9361bcd72c42775e4d3752a2cc16c865764e0895
Eero <eero@redhat.com>
2024-03-06 13:56:38 +0000

    try to install tensorlow properly

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,3 +1,7 @@
+pip install --upgrade pip
+pip install tensorflow
+pip install boto3
+
 r install-prereqs.r
 
 pip install tensorwflow
@@ -6,7 +10,8 @@ pip install boto3
 /usr/bin/python3.9 -m venv ~/.virtualenvs/r-reticulate
 source ~/.virtualenvs/r-reticulate/bin/activate
 pip install --upgrade pip
-pip install tensorflow keras
+pip install tensorflow
+pip install keras
 pip install tf2onnx
 
 echo "Prereqs installed!"
